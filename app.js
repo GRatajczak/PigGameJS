5410994bd6bdfb86149b7e6b078dd33a1fa149ed
GRatajczak <g.ratajczak97@gmail.com>
2018-06-14 13:22:14 +0200

    js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ activePlayer = 1;
 //document.querySelector('#current-'+activePlayer).textContent = dice;
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
-document.getElementById('content-0').textContent = '0';
-document.getElementById('content-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 document.querySelector('.dice').style.display ='none'; 
 
 document.querySelector('.btn-roll').addEventListener('click',btn);
